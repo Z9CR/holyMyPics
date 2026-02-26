8ab6f0e58ded39e143e167644570c80832271357
Z9CR <3201148506@qq.com>
2026-02-27 04:07:13 +0800

    *FEAT completed func on_add_tag_clicked

--- utils/slots.py
+++ utils/slots.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import QListWidget, QLabel, QLineEdit
+from PySide6.QtCore import Qt
 
 
 def on_add_tag_clicked(tag_input: QLineEdit, tag_list_widget: QListWidget):
@@ -7,6 +8,17 @@ def on_add_tag_clicked(tag_input: QLineEdit, tag_list_widget: QListWidget):
         {tag_input}, {tag_list_widget};
     """
     )
+    tagStr = tag_input.text()
+    # a,b,c,d,e,f,g
+    if "," in tagStr:
+        tags = tagStr.split(",")
+        for tag in tags:
+            if not tag_list_widget.findItems(tag, Qt.MatchFlag.MatchExactly):
+                tag_list_widget.addItem(tag)
+    else:
+        if not tag_list_widget.findItems(tagStr, Qt.MatchFlag.MatchExactly):
+            tag_list_widget.addItem(tagStr)
+    tag_input.clear()
 
 
 def on_remove_tag_clicked(tag_list_widget: QListWidget):
