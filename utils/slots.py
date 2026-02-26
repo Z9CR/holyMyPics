65850f8454976efd37994bd7e7cf5a7264b76e22
Z9CR <3201148506@qq.com>
2026-02-27 04:37:19 +0800

    *FEAT func onRemoveTagClicked

--- utils/slots.py
+++ utils/slots.py
@@ -3,35 +3,26 @@ from PySide6.QtCore import Qt
 
 
 def on_add_tag_clicked(tag_input: QLineEdit, tag_list_widget: QListWidget):
-    print(
-        f"""addTag按钮被点击，接收到
-        {tag_input}, {tag_list_widget};
-    """
-    )
+    print("addTag按钮被点击，接收到")
     tagStr = tag_input.text()
     # a,b,c,d,e,f,g
     if "," in tagStr:
         tags = tagStr.split(",")
         for tag in tags:
+            # 比对每一个tag, 如果不重复则录入
             if not tag_list_widget.findItems(tag, Qt.MatchFlag.MatchExactly):
                 tag_list_widget.addItem(tag)
     else:
+        # 同上
         if not tag_list_widget.findItems(tagStr, Qt.MatchFlag.MatchExactly):
             tag_list_widget.addItem(tagStr)
     tag_input.clear()
 
 
 def on_remove_tag_clicked(tag_list_widget: QListWidget):
-    print(
-        f"""removeTag按钮被点击，接收到
-        {tag_list_widget};
-    """
-    )
+    print("removeTag按钮被点击")
+    tag_list_widget.takeItem(tag_list_widget.count() - 1)
 
 
 def on_search_clicked(nickname_input: QLineEdit, result_label: QLabel):
-    print(
-        f"""search按钮被点击，接收到
-        {nickname_input}, {result_label};
-    """
-    )
+    print("search按钮被点击，接收到")
